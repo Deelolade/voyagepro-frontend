c050c25b2837bf8833b38d74a8bf52a47258dc74
Deelolade <oluwanisholahabeeb13@icloud.com>
2025-09-04 20:05:52 +0100

    added the admins apis  dashboard

--- src/components/admin/Dashboard.jsx
+++ src/components/admin/Dashboard.jsx
@@ -1,15 +1,99 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import { FaRegBell, FaRegUserCircle } from 'react-icons/fa'
 import { IoCloudUploadOutline } from 'react-icons/io5';
 import { Link } from 'react-router-dom';
+import { useQueries, } from '@tanstack/react-query'
 
 const Dashboard = () => {
+    const API_URL = import.meta.env.VITE_API_URL;
+    const token = localStorage.getItem('token');
+
+    const fetchTotalUsers = async (token) => {
+        const res = await axios.get(`${API_URL}/admin/dashboard/users`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+        return res.data.totalUsers;
+    }
+    const fetchTotalBookings = async (token) => {
+        const res = await axios.get(`${API_URL}/admin/dashboard/bookings`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+        return res.data.totalBookings;
+    }
+    const fetchTotalRevenue = async (token) => {
+        const res = await axios.get(`${API_URL}/admin/dashboard/revenue`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+        console.log(res.data)
+        return res.data.totalRevenue;
+    }
+    const getTopBookings = async (token) => {
+        const res = await axios.get(`${API_URL}/admin/dashboard/top-bookings`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+        console.log(res.data)
+        return res.data.topPackages;
+    }
+    const getRecentBookings = async (token) => {
+        const res = await axios.get(`${API_URL}/admin/dashboard/recent-bookings`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+        return res.data.recentBookings;
+    }
+    const totalResults = useQueries({
+        queries: [
+            {
+                queryKey: ["totalUsers"],
+                queryFn: () => fetchTotalUsers(token),
+            },
+            {
+                queryKey: ["totalBookings"],
+                queryFn: () => fetchTotalBookings(token),
+            },
+            {
+                queryKey: ["totalRevenue"],
+                queryFn: () => fetchTotalRevenue(token),
+            },
+            {
+                queryKey: ["topBookings"],
+                queryFn: () => getTopBookings(token),
+            },
+            {
+                queryKey: ["recentBookings"],
+                queryFn: () => getRecentBookings(token),
+            },
+
+        ]
+    })
+    const [usersQuery, BookingsQuery, revenueQuery, topBookingQuery, recentBookings] = totalResults;
+
+    console.log(topBookingQuery.data)
+    console.log(recentBookings.data)
+    const formatDate = (dateString) => {
+        return new Date(dateString).toLocaleDateString('en-us', {
+            day: "numeric",
+            month: "short",
+            year: "numeric"
+        })
+    }
     return (
         <div>
             <section className="py-6 px-3 md:px-4  max-h-screen overflow-y-auto scrollbar-hide">
                 <div className="flex justify-between items-center">
                     <h2 className='text-2xl md:text-3xl font-semibold'>Welcome Admin </h2>
                     <div className="flex space-x-6 items-center">
-                        <span><FaRegBell className='text-3xl ' /></span>
+                        <span><FaRegBell className='text-3xl' /></span>
                         <span>< FaRegUserCircle className="scale-150 text-2xl" /></span>
                     </div>
                 </div>
@@ -19,15 +103,15 @@ const Dashboard = () => {
                         <div className="grid md:grid-cols-3 gap-5">
                             <div className="bg-gray rounded-xl px-6 py-6 space-y-3">
                                 <h3 className='text-xl font-medium'>Total Bookings</h3>
-                                <p className='text-2xl font-semibold'>256</p>
+                                <p className='text-2xl font-semibold'>{BookingsQuery?.data ?? 0}</p>
                             </div>
                             <div className="bg-gray rounded-xl px-6 py-6 space-y-3">
                                 <h3 className='text-xl font-medium'>Clients</h3>
-                                <p className='text-2xl font-semibold'>196</p>
+                                <p className='text-2xl font-semibold'>{usersQuery?.data ?? 0}</p>
                             </div>
                             <div className="bg-gray rounded-xl px-6 py-6 space-y-3">
                                 <h3 className='text-xl font-medium'>Revenue</h3>
-                                <p className='text-2xl font-semibold'>#800,789</p>
+                                <p className='text-2xl font-semibold'>#{revenueQuery.data?.toLocaleString() ?? 0}</p>
                             </div>
                         </div>
                     </div>
@@ -59,33 +143,27 @@ const Dashboard = () => {
                     <div className="col-span-2 md:col-span-1 bg-gray border-darkGray/50 border rounded-xl p-6">
                         <h2 className='text-2xl font-medium mb-4'>Bookings</h2>
                     </div>
-
                     <div className="col-span-2 md:col-span-1">
                         <h2 className='text-2xl font-medium ms-3'>Recent Bookings</h2>
                         <div className="col-span-1 bg-gray border-darkGray/50 border rounded-xl p-2 md:p-6 mt-2">
                             <div className="grid grid-cols-3 gap-1 md:gap-3 px-2">
-                                <div className=" space-y-4 text-center">
-                                    <h5 className=' text-lg md:text-xl font-medium'>Date</h5>
-                                    <p className='text-sm'>Jan 14,<span className='hidden md:block'> 2025</span></p>
-                                    <p className='text-sm'>Mar 30,<span className='hidden md:block'> 2025</span></p>
-                                    <p className='text-sm'>Aug 14,<span className='hidden md:block'> 2025</span></p>
-                                    <p className='text-sm'>Dec 08,<span className='hidden md:block'> 2025</span></p>
+                                <h5 className='text-center text-lg font-medium'>Name</h5>
+                                <h5 className='text-center text-lg font-medium'>Date</h5>
+                                <h5 className='text-center text-lg font-medium'>Status</h5>
                                 </div>
-                                <div className=" space-y-4 text-center">
-                                    <h5 className='text-lg md:text-xl font-medium'>Status</h5>
-                                    <p className='text-sm text-green'>Completed</p>
-                                    <p className='text-sm text-green'>Completed</p>
-                                    <p className='text-sm text-orange'>Pending</p>
-                                    <p className='text-sm text-yellow-400'> In Progress</p>
-                                </div>
-                                <div className=" space-y-4 text-center">
-                                    <h5 className='text-lg md:text-xl font-medium'>Destination</h5>
-                                    <p className='text-sm'>Dubai</p>
-                                    <p className='text-sm'>Qatar</p>
-                                    <p className='text-sm'>Bercelona</p>
-                                    <p className='text-sm'>Paris</p>
-                                </div>
-                            </div>
+                                {
+                                    recentBookings?.data
+                                    ?.sort((a, b)=> new Date(b.createdAt) - new Date(a.createdAt))
+                                    .slice(0, 5)
+                                    .map((booking, idx) => (
+                                        <div key={idx} className='grid grid-cols-3 mt-4  '>
+                                            <p className='text-sm text-center flex justify-center items-center'>{booking.packageName.split(" ")[0] }</p>
+                                            <p className='text-sm text-center'>{formatDate(booking.travelDate)}</p>
+                                            <p className={`text-sm font-medium text-center ${booking.bookingStatus.toLowerCase() === "pending" ? "text-orange" : booking.bookingStatus.toLowerCase() === "confirmed" ? "text-green": "text-yellow-500"}  `}>{booking.bookingStatus.charAt(0).toUpperCase() + booking.bookingStatus.slice(1).toLowerCase()}</p>
+                                        </div>
+                                    ))
+                                }
+
                         </div>
                     </div>
                     <Link to='/package' className="relative col-span-2 md:col-span-1">
--- src/main.jsx
+++ src/main.jsx
@@ -6,13 +6,29 @@ import { BrowserRouter } from "react-router-dom";
 import { persistor, store } from "../src/redux/store.js";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: true,
+      refetchOnMount: false,
+      staleTime: 5 * 60 * 1000, // 5 minutes
+      cacheTime: 30 * 60 * 1000, // 30 minutes
+      retry: 1,
+    }
+  }
+});
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate>
-    </Provider>
-  </BrowserRouter>
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
+    </BrowserRouter>
+  </QueryClientProvider>
 );
